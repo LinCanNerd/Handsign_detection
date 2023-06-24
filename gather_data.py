5e9f2a90345627b4c79ad0bb54fe11a28b1d7274
LinCannn <LincanECDL@gmail.com>
2023-06-24 20:49:24 +0200

    BELLA CIAO
    BELLA CIAO

---
+++ gather_data.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import mediapipe as mp
+
+
+mp_holistic = mp.solutions.holistic  # Holistic model
+mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False  # Image is no longer writeable
+    results = model.process(image)  # Make prediction
+    image.flags.writeable = True  # Image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)  # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)  # Draw right hand connections
+
+
+def draw_styled_landmarks(image, results):
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                             )
+    # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                             )
+
+
+def extract_keypoints(results):
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([lh, rh])
+
+
+# Path for exported data
+DATA_DIR = r"C:\Users\Lin Can\Desktop\Handsign project\Handsign_detection\data"
+
+number_of_classes = 24
+dataset_size = 500
+
+
+
+cap = cv2.VideoCapture(0)
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    
+    for j in range(number_of_classes):
+        if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+            os.makedirs(os.path.join(DATA_DIR, str(j)))
+
+        print('Collecting data for class {}'.format(j))
+
+        done = False
+        while True:
+            ret, frame = cap.read()
+            image, results = mediapipe_detection(frame, holistic)
+            draw_styled_landmarks(image, results)
+            cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                        cv2.LINE_AA)
+            cv2.imshow('frame', image)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        counter = 0
+        while counter < dataset_size:
+            
+            ret, frame = cap.read()
+            image, results = mediapipe_detection(frame, holistic)
+            draw_styled_landmarks(image, results)
+            cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(j, counter), (15,12), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.imshow('frame', image)
+            cv2.waitKey(1)
+            cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+
+            counter += 1
+    print("COLLECTION OVER!")
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
