3f57657cb26750029960dc30f14c4060bbd387e8
LinCannn <LincanECDL@gmail.com>
2023-11-15 09:39:45 +0100

    final

---
+++ main/restart.py
@@ -0,0 +1,8 @@
+from inspect import getsourcefile
+from os.path import abspath
+import os
+import playsound
+def start():
+    sound_path = os.path.dirname(abspath(getsourcefile(lambda:0)))
+    playsound.playsound(sound_path+ '\start.mp3')
+#start()
--- restart.py
+++
@@ -1,8 +0,0 @@
-from inspect import getsourcefile
-from os.path import abspath
-import os
-import playsound
-def start():
-    sound_path = os.path.dirname(abspath(getsourcefile(lambda:0)))
-    playsound.playsound(sound_path+ '\start.wav')
-#start()
